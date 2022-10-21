107cb4e7cd4872cfb750e678d7d5fe164547151c
Arjun31415 <arjunp0710@gmail.com>
2022-10-21 20:12:21 +0530

    initial form and web server

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/HackSquadDev/contributor-of-the-xxx-golang
 
 go 1.19
+
+require github.com/joho/godotenv v1.4.0
--- main.go
+++ main.go
@@ -1,7 +1,52 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/joho/godotenv"
+	"log"
+	"net/http"
+	"os"
+	"strconv"
+)
 
+func homeHandler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/home" {
+		http.Error(w, "Oops requested URL not found", http.StatusNotFound)
+		return
+	}
+	if r.Method != "GET" {
+		http.Error(w, "Method is not supported", http.StatusMethodNotAllowed)
+	}
+	fmt.Fprintf(w, "Welcome Home!")
+}
+func formHandler(w http.ResponseWriter, r *http.Request) {
+	if err := r.ParseForm(); err != nil {
+		fmt.Fprintf(w, "Form Parsing Error : %v", err)
+		return
+	}
+	fmt.Fprintf(w, "Form submitted successfully\n")
+	orgUrl := r.FormValue("orgURL")
+	fmt.Fprintf(w, "Organization URL: %s", orgUrl)
+}
 func main() {
-	fmt.Printf("Let's go!")
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	port, err := strconv.Atoi(os.Getenv("PORT"))
+	if err != nil {
+		panic(err)
+	}
+	fileServer := http.FileServer(http.Dir("./static"))
+	http.Handle("/", fileServer)
+	http.HandleFunc("/home", homeHandler)
+	http.HandleFunc("/submit", formHandler)
+
+	fmt.Printf("Starting HTTP server on port %d\n", port)
+
+	if err := http.ListenAndServe(fmt.Sprintf(":%d", port), nil); err != nil {
+		log.Fatal(err)
+	}
+
 }
