e14c7810a71fd916dbe51055338853056ce3c2ef
Isfer Hossain <midrashahil123@gmail.com>
2022-10-22 18:15:36 +0600

    Added: basic GraphQL support with example query.

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
 	"net/http"
@@ -9,9 +10,11 @@ import (
 
 	"github.com/joho/godotenv"
 	"github.com/labstack/echo/v4"
+	"github.com/machinebox/graphql"
 )
 
 func main() {
+	// Load ENVs
 	err := godotenv.Load(".env")
 	if err != nil {
 		log.Fatal("Error loading .env file")
@@ -21,7 +24,7 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-
+	// Initialize Echo
 	e := echo.New()
 
 	e.GET("/", home)
@@ -30,5 +33,38 @@ func main() {
 }
 
 func home(c echo.Context) error {
-	return c.HTML(http.StatusOK, "<h1>Home!</h1>")
+	client := graphql.NewClient("https://api.github.com/graphql")
+	response := requestData(client)
+	return c.HTML(http.StatusOK, response.Licenses[0].Name)
+}
+
+// GraphQL Related Functions
+func requestData(client *graphql.Client) GitHubResponse {
+	query := `
+	{
+		licenses {
+			name
+			featured
+		}
+	}
+	`
+	request := graphql.NewRequest(query)
+	githubToken := os.Getenv("GITHUB_TOKEN")
+	if githubToken == "" {
+		log.Fatal("GITHUB_TOKEN is empty.")
+	}
+	request.Header.Set("Authorization", fmt.Sprintf("bearer %s", githubToken))
+	var resp GitHubResponse
+	err := client.Run(context.Background(), request, &resp)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return resp
+}
+
+type GitHubResponse struct {
+	Licenses []struct {
+		Name     string
+		Featured bool
+	}
 }
