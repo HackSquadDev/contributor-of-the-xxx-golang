5512e68d929ddeeee8255e9f04652e94375f219c
Arjun31415 <arjunp0710@gmail.com>
2022-10-29 11:24:51 +0530

    added verbose logs

--- imageExtractor/imageExtractor.go
+++ imageExtractor/imageExtractor.go
@@ -30,31 +30,39 @@ func Setup() string {
 func GetImage() {
 	// Setup
 	url := Setup()
+	log.Default().Println("Playwright setup successful")
+	log.Default().Println("URL: ", url)
 	// Launch playwright
 	pw, err := playwright.Run()
 	if err != nil {
 		panic(err)
 	}
+	log.Default().Println("Playwright running successfully")
 	// Launch browser
 	browser, err := pw.Chromium.Launch()
 	if err != nil {
 		panic(err)
 	}
+	log.Default().Println("Chromium Browser launched successfully")
 	// Launch context
 	context, err := browser.NewContext()
 	if err != nil {
 		panic(err)
 	}
+	log.Default().Println("Context launched successfully")
 	// Launch page
 	page, err := context.NewPage()
 	if err != nil {
 		panic(err)
 	}
+	log.Default().Println("Page launched successfully")
 	// Navigate to url
 	_, err = page.Goto(url)
 	if err != nil {
 		panic(err)
 	}
+	log.Default().Println("Page navigated successfully")
+	log.Default().Println("Waiting for div to load")
 	// wait max 200 sec for div with class card
 	_, err = page.WaitForSelector(".card", playwright.PageWaitForSelectorOptions{
 		Timeout: playwright.Float(200000),
@@ -62,6 +70,7 @@ func GetImage() {
 	if err != nil {
 		panic(err)
 	}
+	log.Default().Println("div with class=\"card\" loaded successfully")
 	// wait till div with class card is present, timeout 200 sec
 	_, err = page.WaitForSelector(".card", playwright.PageWaitForSelectorOptions{
 		Timeout: playwright.Float(200000),
@@ -69,12 +78,14 @@ func GetImage() {
 	if err != nil {
 		panic(err)
 	}
+	log.Default().Println("div with class=\"card\" is present")
 	val, err := page.Locator(".card", playwright.PageLocatorOptions{})
 	if err != nil {
 		panic(err)
 	}
-	// omit background color in screenshot
+	log.Default().Println("found div with class card")
 
+	// omit background color in screenshot
 	_, err = val.Screenshot(playwright.LocatorScreenshotOptions{
 		// save to file with today's date
 		Path:           playwright.String("images/" + time.Now().Format("2006-01-02") + ".png"),
@@ -83,11 +94,14 @@ func GetImage() {
 	if err != nil {
 		panic(err)
 	}
+	log.Default().Println("saved image to file")
+	log.Default().Println("Closing browser")
 	// close the browser
 	err = browser.Close()
 	if err != nil {
 		panic(err)
 	}
+	log.Println("closed browser. Done!")
 
 }
 func main() {
