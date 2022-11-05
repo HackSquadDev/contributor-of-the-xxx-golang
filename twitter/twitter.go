2ed24e10cd3768b3cef0da1eb1ff56d760a50df2
Arjun31415 <arjunp0710@gmail.com>
2022-11-05 14:09:30 +0530

    some updates ig

--- twitter/twitter.go
+++ twitter/twitter.go
@@ -2,41 +2,53 @@ package twitter
 
 import (
 	"bytes"
-	"encoding/json"
 	"io"
 	"log"
+	"mime/multipart"
 	"net/http"
 	"os"
+	"strings"
 )
 
 func PostToTwitter(msg string, imgPath string) {
 
 	// get TWITTER_API_URL from .env
 	twitterApiUrl := os.Getenv("TWITTER_API_URL")
-	// open the imgPath file to send to twitter
+	var b bytes.Buffer
+
+	w := multipart.NewWriter(&b)
+	// use multipart writer to read image from path
+
 	img, err := os.Open(imgPath)
 	if err != nil {
 		panic(err)
 	}
+	fw, err := w.CreateFormFile("upload", imgPath)
+	if err != nil {
+		panic(err)
+	}
+	if fw, err = w.CreateFormField("text"); err != nil {
+		panic(err)
+	}
+	if _, err = io.Copy(fw, strings.NewReader(msg)); err != nil {
+		panic(err)
+	}
+	w.Close()
 	log.Default().Println("Image opened successfully")
 	defer img.Close()
 
-	postBody, err := json.Marshal(map[string]interface{}{
-		"text":   msg,
-		"upload": img,
-	})
 	if err != nil {
 		panic(err)
 	}
 	log.Default().Println("Post body created successfully")
 	// make POST request to the url, with the body
 	// and the auth header
-	req, err := http.NewRequest("POST", twitterApiUrl, bytes.NewBuffer(postBody))
+	req, err := http.NewRequest(http.MethodPost, twitterApiUrl, &b)
 	if err != nil {
 		panic(err)
 	}
 	defer req.Body.Close()
-	req.Header.Set("Content-Type", "application/json")
+	req.Header.Set("Content-Type", w.FormDataContentType())
 	// send request
 	client := &http.Client{}
 	resp, err := client.Do(req)
